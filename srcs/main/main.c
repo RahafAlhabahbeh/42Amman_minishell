31a6c79853ac83e1fbc74e5d47a4d600fc8c35a5
Dana Al-Mahrouk <dal-mahr@c1r4s3.42amman.com>
2025-07-27 12:48:25 +0300

    TOK -1

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -3,14 +3,79 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dal-mahr <dal-mahr@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: dal-mahr <dal-mahr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 22:14:22 by aaljazza          #+#    #+#             */
-/*   Updated: 2025/07/24 08:53:51 by dal-mahr         ###   ########.fr       */
+/*   Updated: 2025/07/27 12:46:09 by dal-mahr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+void print_tokens(t_token *token)
+{
+    printf("\n🧩 Token List:\n");
+    while (token)
+    {
+        const char *type_str;
+        switch (token->type)
+        {
+            case WORD: type_str = "WORD"; break;
+            case PIPE: type_str = "PIPE"; break;
+            case REDIR_IN: type_str = "REDIR_IN"; break;
+            case REDIR_OUT: type_str = "REDIR_OUT"; break;
+            case REDIR_APPEND: type_str = "REDIR_APPEND"; break;
+            case HERE_DOC: type_str = "HERE_DOC"; break;
+            default: type_str = "UNKNOWN"; break;
+        }
+
+        printf("  [%s] \"%s\" (quote: %c)\n", type_str, token->value, token->quote ? token->quote : '-');
+        token = token->next;
+    }
+    printf("🔚 End of Tokens\n");
+}
+
+
+void print_commands(t_cmd *cmd)
+{
+    int i = 0;
+    printf("\n🚀 Command List:\n");
+    while (cmd)
+    {
+        printf("  Cmd[%d]:\n", i);
+        if (cmd->argv)
+        {
+            for (int j = 0; cmd->argv[j]; j++)
+                printf("    argv[%d]: [%s]\n", j, cmd->argv[j]);
+        }
+        else
+            printf("    (no argv)\n");
+
+        // Input redirection
+        switch (cmd->in_type)
+        {
+            case REDIR_IN: printf("    input: REDIR_IN <%s>\n", cmd->input_file_name); break;
+            case HERE_DOC: printf("    input: HERE_DOC\n"); break;
+            case PIPE:     printf("    input: PIPE\n"); break;
+            default:       printf("    input: none\n"); break;
+        }
+
+        // Output redirection
+        switch (cmd->out_type)
+        {
+            case REDIR_OUT:    printf("    output: REDIR_OUT >%s\n", cmd->output_file_name); break;
+            case REDIR_APPEND: printf("    output: APPEND >>%s\n", cmd->output_file_name); break;
+            case PIPE:         printf("    output: PIPE\n"); break;
+            default:           printf("    output: none\n"); break;
+        }
+
+        cmd = cmd->next;
+        i++;
+    }
+    printf("🔚 End of Commands\n");
+}
+
+
 
 int main(int ac, char **av, char **envp)
 {
@@ -36,6 +101,10 @@ int main(int ac, char **av, char **envp)
         t_token *old = minishell.token;
         minishell.token = expand(&minishell);
         free_tokens(old);
+
+        print_tokens(minishell.token);
+
+        
         count_pipe(&minishell);
         if (minishell.cmd)
             free_commands(&minishell);
@@ -45,6 +114,9 @@ int main(int ac, char **av, char **envp)
             exit(1);
 
         put_token_to_commands(&minishell);
+
+        print_commands(minishell.cmd);
+        
         execute_command(&minishell, envp);
 
 
